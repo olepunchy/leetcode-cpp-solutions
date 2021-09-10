e0cd3bbebc2383b751bd32a8f1be032238684bd4
Jeremy Novak <olepunchy@gmail.com>
2021-09-10 16:27:56 -0500

    Added first_bad_version problem

---
+++ first_bad_version/build/CMakeFiles/first_bad_version.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/first_bad_version.dir/main.cpp.o"
+  "CMakeFiles/first_bad_version.dir/main.cpp.o.d"
+  "first_bad_version"
+  "first_bad_version.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/first_bad_version.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
